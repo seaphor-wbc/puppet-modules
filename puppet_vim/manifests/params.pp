d4603a455c815cf2fffb8da20aeff833d5474367
SeaPhor <seaphor@woodbeeco.com>
2017-11-06 06:59:38 -0600

    Added puppet_vim module, adjusted it to work with SUSE*, still needs some adjusting but it works

---
+++ puppet_vim/manifests/params.pp
@@ -0,0 +1,34 @@
+#Params pattern used to calculate and store variables for installing
+#vim syntax files for Puppet
+class puppet_vim::params {
+  case $::osfamily {
+    'Suse': {
+      $vimdir       = '/usr/share/vim/vim74/'
+      $vimrc        = '/etc/vimrc'
+      #$vimrc_source = 'redhat_vimrc'
+      $vimrc_source = 'suse_vimrc'
+      $vim_package  = 'vim'
+    }
+    'RedHat': {
+      $vimdir       = '/usr/share/vim/vim74/'
+      $vimrc        = '/etc/vimrc'
+      $vimrc_source = 'redhat_vimrc'
+      $vim_package  = 'vim-enhanced'
+    }
+    'Debian': {
+      $vimdir       = '/etc/vim/'
+      $vimrc        = '/etc/vim/vimrc.local'
+      $vimrc_source = 'debian_vimrc'
+      $vim_package  = 'vim'
+    }
+    'Solaris': {
+      $vimdir       = '/usr/share/vim/vim73/'
+      $vimrc        = '/usr/share/vim/vimrc'
+      $vimrc_source = 'solaris_vimrc'
+      $vim_package  = 'vim'
+    }
+    default: {
+      fail( "Module ${module_name} isn't tested on ${::osfamily} based systems")
+    }
+  }
+}
